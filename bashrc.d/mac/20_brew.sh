68d373040d94248afd3a74195150e9018fc2174b
A. Ishikawa <a.ishikawa810@gmail.com>
2015-12-29 22:48:42 +0900

    Remove export installed list of brew

--- bashrc.d/mac/20_brew.sh
+++ bashrc.d/mac/20_brew.sh
@@ -5,14 +5,3 @@ command_exists brew || return 0
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
     . $(brew --prefix)/etc/bash_completion
 fi
-
-function brew() {
-    trap "return" EXIT
-    `which brew` $@
-    for option in $@
-    do
-	if [ "$option" = "install" ]; then
-	    brew list > $BREW_INSTALLED_PACKAGES_FILE
-	fi
-    done
-}
