7314b39a6637bc4b8e3aa0e37b4b47af5bfb0c2f
A. Ishikawa <a.ishikawa810@gmail.com>
2016-04-09 22:24:29 +0900

    Add .gitignore file and install script.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+exclude_files=(
+    .git
+    README.md
+    LICENSE
+    $(basename $0)
+)
+
+dotfiles_directory=$(dirname $0)
+current_directory=$(pwd)
+trap "cd $current_directory" EXIT
+
+cd $dotfiles_directory
+dotfiles_directory=`pwd`
+
+for file in .??*
+do
+    for exclude_file in ${exclude_files[@]}
+    do
+        [ "${file}" == "${exclude_file}" ] && continue 2
+    done
+
+    ln -s $dotfiles_directory/$file $HOME/$file
+done
