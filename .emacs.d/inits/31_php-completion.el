b9ba6c0b606bedf873629edaa35b490a6dc6890d
A. Ishikawa <a.ishikawa810@gmail.com>
2017-02-05 16:07:44 +0900

    Change configuration for Emacs 25.1

--- .emacs.d/inits/31_php-completion.el
+++
@@ -1,7 +0,0 @@
-(add-hook 'php-mode-hook
-	  #'(lambda()
-	      (require 'php-completion)
-	      (php-completion-mode t)
-	      (add-to-list 'ac-sources 'ac-source-php-completion)
-	      )
-	  )
