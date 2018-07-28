7646acb4151b5dcba592d7cfcb19392f11a80c83
A. Ishikawa <a.ishikawa810@gmail.com>
2018-07-28 17:15:26 +0900

    Configure path for php

--- bash/bash_profile.d/mac/20_brew.sh
+++ bash/bash_profile.d/mac/20_brew.sh
@@ -7,3 +7,9 @@ fi
 command_exists brew || return 0
 
 export BREW_INSTALLED_PACKAGES_FILE=$HOME/.homebrew.list
+
+# php7.1
+PHP_PATH=$(brew --prefix php@7.1 2>/dev/null)
+if [ "$PHP_PATH" != "" ]; then
+    PATH=$PHP_PATH:$PATH
+fi
