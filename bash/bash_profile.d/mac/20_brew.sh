24eec4fe010b61af94763dcc0ce59809dd898f04
A. Ishikawa <a.ishikawa810@gmail.com>
2018-11-29 15:39:37 +0900

    [Bash] Install git completions

--- bash/bash_profile.d/mac/20_brew.sh
+++ bash/bash_profile.d/mac/20_brew.sh
@@ -10,6 +10,15 @@ export BREW_INSTALLED_PACKAGES_FILE=$HOME/.homebrew.list
 
 [ -f $(brew --prefix)/etc/bash_completion ] && . $(brew --prefix)/etc/bash_completion
 
+if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
+fi
+
+if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+  PS1='$(__git_ps1 "git(%s)")$ '
+fi
+
 # PHP_PATH=$(brew --prefix php@7.1 2>/dev/null)
 # if [ "$PHP_PATH" != "" ]; then
 #     PATH=$PHP_PATH/bin:$PATH
