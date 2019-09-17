5cf3db7b14f18061310a31f424009152d0a4db2f
A. Ishikawa <a.ishikawa810@gmail.com>
2019-09-16 22:10:50 -0700

    Update fish prompt and iTerm config bar

---
+++ static/common/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,19 @@
+function fish_prompt
+    set -l current_k8s_context (kubectl config current-context)
+    set -l gcp_project (cat ~/.config/gcloud/active_config)
+    #    set -l gcp_project (cat ~/.config/gcloud/configurations/config_(cat ~/.config/gcloud/active_config) | grep project | cut -f 3 -d " ")
+
+    set -x envs
+    if contains $current_k8s_context $FISH_PROMPT_K8S_CONTEXT_NAMES
+        set envs $envs (printf 'k8s:%s' $current_k8s_context)
+    end
+    if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
+        set envs $envs (printf 'GCP:%s' $gcp_project)
+    end
+    if [ (count $envs) -gt 0 ]
+        set_color red
+        printf '(%s) ' (string join ',' $envs)
+        set_color normal
+    end
+    printf '> '
+end
