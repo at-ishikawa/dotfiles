f7c5f42cdde35b392b8374d04041febabd4bfeba
A. Ishikawa <a.ishikawa810@gmail.com>
2019-09-29 21:44:37 -0700

    Fish: Update prompt to make them minimize

--- static/common/.config/fish/functions/fish_prompt.fish
+++ static/common/.config/fish/functions/fish_prompt.fish
@@ -5,14 +5,14 @@ function fish_prompt
 
     set -x envs
     if contains $current_k8s_context $FISH_PROMPT_K8S_CONTEXT_NAMES
-        set envs $envs (printf 'k8s:%s' $current_k8s_context)
+        set envs $envs (printf '⎈ %s' $current_k8s_context)
     end
     if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
         set envs $envs (printf 'GCP:%s' $gcp_project)
     end
     if [ (count $envs) -gt 0 ]
         set_color red
-        printf '(%s) ' (string join ',' $envs)
+        printf '%s ' (string join ',' $envs)
         set_color normal
     end
     printf '> '
