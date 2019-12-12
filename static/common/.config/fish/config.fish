fc5cd1dc611748e3f5ae27d697fa336b922088f2
A. Ishikawa <a.ishikawa810@gmail.com>
2019-12-11 22:31:51 -0800

    fish: update prompt

--- static/common/.config/fish/config.fish
+++ static/common/.config/fish/config.fish
@@ -3,6 +3,16 @@ set -U FZF_TMUX 1
 set -U FZF_TMUX_HEIGHT "40%"
 set -U FZF_DEFAULT_OPTS "--bind ctrl-k:kill-line --height $FZF_TMUX_HEIGHT"
 
+# fish_prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+# set -g __fish_git_prompt_color -b grey
+# set -g __fish_git_prompt_color_branch -b grey yellow
+set __fish_git_prompt_color_upstream_ahead -b grey green
+set __fish_git_prompt_color_upstream_behind -b grey red
+set __fish_git_prompt_show_informative_status 'yes'
 
 # Golang
 set -x GOPATH $HOME/go
