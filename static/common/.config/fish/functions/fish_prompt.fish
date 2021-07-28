1f1eecd9d5f2736369d6d1fde394c4affa0bb732
A. Ishikawa <a.ishikawa810@gmail.com>
2021-07-28 14:54:24 +0900

    Fish: Fix not to show error messages when there is no context for kubectl

--- static/common/.config/fish/functions/fish_prompt.fish
+++ static/common/.config/fish/functions/fish_prompt.fish
@@ -8,8 +8,11 @@ function fish_right_prompt
     #    set -l gcp_project (cat ~/.config/gcloud/configurations/config_(cat ~/.config/gcloud/active_config) | grep project | cut -f 3 -d " ")
 
     set -x envs
-    if contains $current_k8s_context $FISH_PROMPT_K8S_CONTEXT_NAMES
-        set envs $envs (printf '⎈ %s' $current_k8s_context)
+    if type -q kubectl
+	set -l current_k8s_context (kubectl config current-context 2>/dev/null)
+	if contains "$current_k8s_context" $FISH_PROMPT_K8S_CONTEXT_NAMES
+            set envs $envs (printf '⎈ %s' $current_k8s_context)
+	end
     end
     if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
         set envs $envs (printf 'GCP:%s' $gcp_project)
