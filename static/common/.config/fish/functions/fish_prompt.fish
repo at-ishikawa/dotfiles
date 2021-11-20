b6486a4c629502aa7e29062f7efd571251895b0e
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2021-11-19 22:22:44 -0800

    Fish: Fix to update fish prompt not to use fish_right_prompt

--- static/common/.config/fish/functions/fish_prompt.fish
+++ static/common/.config/fish/functions/fish_prompt.fish
@@ -1,27 +1,8 @@
-<<<<<<< Updated upstream
-function fish_prompt
-    echo '> '
-end
-
-function fish_right_prompt
-    set -x envs
-    if type -q kubectl
-	set -l current_k8s_context (kubectl config current-context 2>/dev/null)
-	if contains "$current_k8s_context" $FISH_PROMPT_K8S_CONTEXT_NAMES
-            set envs $envs (printf '⎈ %s' $current_k8s_context)
-	end
-    end
-    if test -f ~/.config/gcloud/active_config
-	set -l gcp_project (cat ~/.config/gcloud/active_config)
-	if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
-            set envs $envs (printf 'GCP:%s' $gcp_project)
-	end
-    end
-
-    if [ (count $envs) -gt 0 ]
-        echo '['
-=======
 function __fish_prompt_kubernetes
+    if not type -q kubectl
+	return
+    end
+
     set -l current_k8s_context (kubectl config current-context)
     set -l prompt_message '⎈ '$current_k8s_context
 
@@ -35,11 +16,14 @@ function __fish_prompt_kubernetes
 end
 
 function __fish_prompt_gcp
+    if not test -f ~/.config/gcloud/active_config
+	return
+    end
+
     set -l gcp_project (cat ~/.config/gcloud/active_config)
     set -l prompt_message 'GCP '$gcp_project
 
     if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
->>>>>>> Stashed changes
         set_color red
         echo -n $prompt_message
         set_color normal
