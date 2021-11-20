4c5c15fa49313af4651daaa1afb7e2705d91980d
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2021-11-19 22:18:20 -0800

    Fish: Update fish prompt not to use fish_right_prompt

--- static/common/.config/fish/functions/fish_prompt.fish
+++ static/common/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function fish_prompt
     echo '> '
 end
@@ -19,10 +20,58 @@ function fish_right_prompt
 
     if [ (count $envs) -gt 0 ]
         echo '['
+=======
+function __fish_prompt_kubernetes
+    set -l current_k8s_context (kubectl config current-context)
+    set -l prompt_message '⎈ '$current_k8s_context
+
+    if contains $current_k8s_context $FISH_PROMPT_K8S_CONTEXT_NAMES
         set_color red
-        printf '%s' (string join ',' $envs)
+        echo -n $prompt_message
         set_color normal
-        echo ']'
+    else
+        echo -n $prompt_message
     end
-    __fish_git_prompt
+end
+
+function __fish_prompt_gcp
+    set -l gcp_project (cat ~/.config/gcloud/active_config)
+    set -l prompt_message 'GCP '$gcp_project
+
+    if contains $gcp_project $FISH_PROMPT_GCP_CONFIG_NAMES
+>>>>>>> Stashed changes
+        set_color red
+        echo -n $prompt_message
+        set_color normal
+    else
+        echo -n $prompt_message
+    end
+end
+
+function __fish_prompt_status
+    set -l display_status $argv[1]
+    set -l prompt_message (printf 'status: %s' $display_status)
+
+    if [ $display_status != 0 ]
+        set_color $fish_color_error
+        echo -n $prompt_message
+        set_color normal
+    else
+        echo -n $prompt_message
+    end
+end
+
+function fish_prompt
+    set -l display_status $status
+
+    echo
+    string join ' ' \
+        (printf '(%s)' $PWD) \
+        (printf '(%s)' (__fish_prompt_kubernetes)) \
+        (printf '(%s)' (__fish_prompt_gcp)) \
+#         (prompt_pwd) \
+        (__fish_git_prompt) \
+        (printf '(%s)' (__fish_prompt_status $display_status)) \
+
+    echo '> '
 end
