d5f5c20b444499246717fba4bcfb90ca1ccc8e73
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2024-08-10 20:33:19 -0700

    misc: Update after Ubuntu 24.04 WSL setup

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -1,18 +1,23 @@
 # Install WSL: https://learn.microsoft.com/en-us/windows/wsl/install
-wsl --install
+# The list of available distributions by wsl --list --online
+wsl --install -d Ubuntu-24.04
 
+# Search packages by winget search $keyword
 $packages = @(
-   "google.chrome",
-   "google.drive",
-   "1Password",
-   "powertoys",
-   "Auto Dark Mode", #https://github.com/AutoDarkMode/Windows-Auto-Night-Mode
-   "google.japaneseime",
+    "Google Drive",
+    "1Password",
+    "powertoys",
+    "Auto Dark Mode", #https://github.com/AutoDarkMode/Windows-Auto-Night-Mode
+    "google.japaneseime",
 
-    "canonical.ubuntu.2204",
+    # "canonical.ubuntu.2204",
     "vscode",
-   "git.git",
-   "Docker"
+    "git.git",
+    "Docker",
+
+    # Some applications are managed by Steam
+    # 1. WallPaper Engine
+    "Steam"
 )
 foreach ($package in $packages) {
     echo "Package: $package"
--- static/common/.config/fish/config_linux.fish
+++ static/common/.config/fish/config_linux.fish
@@ -4,3 +4,7 @@ set -g PATH $PATH $GOOGLE_CLOUD_SDK_ROOT/bin
 
 set -g GOROOT /usr/local/go
 set -g PATH $PATH $GOROOT/bin
+
+# Configure docker CLI from docker desktop
+# https://github.com/docker/for-win/issues/13088#issuecomment-1536365076
+set -g PATH $PATH /mnt/wsl/docker-desktop/cli-tools/usr/bin/
